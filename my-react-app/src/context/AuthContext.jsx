1ba23d2393a3829b56aed214a31ff2b696fee8fb
Cj Obi <cj.obi@ucalgary.ca>
2026-03-26 13:27:22 -0600

    Created a case allowing user data to be saved when logged in to localStorage

--- my-react-app/src/context/AuthContext.jsx
+++ my-react-app/src/context/AuthContext.jsx
@@ -2,7 +2,15 @@ import {createContext, useContext, useEffect, useMemo, useState} from 'react';
 
 const AuthContext = createContext(null);
 
- export function AuthProvider({children}) {
+export function AuthProvider({children}) {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => { 
+        const savedUser = localStorgae.getItem('user');
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+    },[]);
 
 
 
@@ -10,5 +18,4 @@ const AuthContext = createContext(null);
 
 
 
-    
  }
