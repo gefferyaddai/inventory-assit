f09742691acd98c83c1e7a048a8be24d4b521fb1
Cj Obi <cj.obi@ucalgary.ca>
2026-03-26 13:15:37 -0600

    Working on main layout of the authentication

--- my-react-app/src/context/AuthContext.jsx
+++ my-react-app/src/context/AuthContext.jsx
@@ -1,3 +1,14 @@
 import {createContext, useContext, useEffect, useMemo, useState} from 'react';
 
 const AuthContext = createContext(null);
+
+ export function AuthProvider({children}) {
+
+
+
+
+
+
+
+    
+ }
