d3ad6b22643bb8070b8080b970a7b1cc72a8e417
Cj Obi <cj.obi@ucalgary.ca>
2026-03-26 13:29:14 -0600

    Created the fuctionality for login seting user data in local storage

--- my-react-app/src/context/AuthContext.jsx
+++ my-react-app/src/context/AuthContext.jsx
@@ -12,6 +12,11 @@ export function AuthProvider({children}) {
         }
     },[]);
 
+    const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+    };
+
 
 
 
