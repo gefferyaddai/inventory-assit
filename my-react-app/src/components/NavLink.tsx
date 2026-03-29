ed42ec06c15cc7643fb044c1c34e45f35863529d
Cj Obi <cj.obi@ucalgary.ca>
2026-03-28 20:27:21 -0600

    Created the Navigation link for each tab

---
+++ my-react-app/src/components/NavLink.tsx
@@ -0,0 +1,31 @@
+import { NavLink as RouterNavLink, type NavLinkProps } from "react-router-dom";
+import { forwardRef } from "react";
+
+interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
+  className?: string;
+  activeClassName?: string;
+  pendingClassName?: string;
+}
+
+function mergeClasses(...classes: Array<string | false | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
+  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+    return (
+      <RouterNavLink
+        ref={ref}
+        to={to}
+        className={({ isActive, isPending }) =>
+          mergeClasses(className, isActive && activeClassName, isPending && pendingClassName)
+        }
+        {...props}
+      />
+    );
+  },
+);
+
+NavLink.displayName = "NavLink";
+
+export { NavLink };
